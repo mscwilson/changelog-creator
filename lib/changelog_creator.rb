33095b6df43fd46ba785b7c3dc9354b06558f3d7
Miranda Wilson <miranda@snowplowanalytics.com>
2022-03-09 23:36:49 +0000

    Generate a list of new commits

--- lib/changelog_creator.rb
+++ lib/changelog_creator.rb
@@ -1,15 +1,21 @@
 require "json"
 
-puts "Hello, #{ARGV[0]}"
+# puts "Hello, #{ARGV[0]}"
 
 class ChangelogCreator
 
-  COMMIT_PATTERN = /([\w\s\.,'"-:\`@]+)\((?:close|closes|fixes|fix) (\#\d+)\)/
+  COMMIT_PATTERN = /\A([\w\s\.,'"-:\`@]+)\((?:close|closes|fixes|fix) (\#\d+)\)$/
 
   def read_commits(file_path)
     raise StandardError.new "Must be a JSON file" if file_path[-5..-1] != ".json"
 
     JSON.parse(File.read(file_path))
+  end
+
+  def read_changelog(file_path)
+    raise StandardError.new "Must be a changelog file" if file_path[-9..-1] != "CHANGELOG"
+
+    File.open(file_path).readlines.map(&:chomp)
   end
 
   def parse_message(message)
@@ -18,11 +24,18 @@ class ChangelogCreator
   end
 
   def extract_messages(json)
-    p json.map { |commit| parse_message(commit["commit"]["message"]) }.compact
+    json.map { |commit| parse_message(commit["commit"]["message"]) }.compact
   end
 
   def make_header(version)
     "#{version} (#{Date.today.strftime('%Y-%m-%d')})"
+  end
+
+  def generate_log_for_new_commits(commits_path, changelog_path, version)
+    commits = extract_messages(read_commits(commits_path))
+    existing_changelog = read_changelog(changelog_path)
+    new_commits = commits.take_while { |message| !existing_changelog.include? message }
+    "#{make_header(version)}\n-----------------------\n#{new_commits.join("\n")}\n"
   end
 
 end
--- spec/changelog_creator_spec.rb
+++ spec/changelog_creator_spec.rb
@@ -5,6 +5,7 @@ describe ChangelogCreator do
 
   before do
     @creator = ChangelogCreator.new
+    allow(Date).to receive(:today).and_return(Date.new(2022, 2, 1))
   end
 
   it "gets commits from a file" do
@@ -13,7 +14,7 @@ describe ChangelogCreator do
     expect(results[0]["commit"]["author"]["name"]).to eq "Miranda Wilson"
   end
 
-  it "checks file extension is valid" do
+  it "checks commits file extension is valid" do
     filename = "./lib/changelog_creator.rb"
     expect { @creator.read_commits(filename) }.to raise_error(StandardError)
   end
@@ -24,6 +25,7 @@ describe ChangelogCreator do
     messages = @creator.extract_messages(json)
     expect(messages.length).to eq 25
     expect(messages[0]).to eq "Attribute community contributions in changelog (#289)"
+    expect(messages[1]).to eq "Remove logging of user supplied values (#286)"
   end
 
   it "parses commit messages" do
@@ -37,15 +39,26 @@ describe ChangelogCreator do
     expect(@creator.parse_message(message)).to be nil
   end
 
+  it "reads an existing changelog" do
+    filename = "./lib/example_CHANGELOG"
+    results = @creator.read_changelog(filename)
+    expect(results[0]).to eq "Java 0.11.0 (2021-12-14)"
+  end
 
+  # would be better to extract this from the tags
+  # use the GH API
   it "makes a version header" do
     version = "Java 0.12.0"
-    allow(Date).to receive(:today).and_return(Date.new(2022, 2, 1))
     expect(@creator.make_header(version)).to eq "Java 0.12.0 (2022-02-01)"
   end
 
+  it "creates formatted commits for the current release" do
+    commits = "./lib/example_commits.json"
+    changelog = "./lib/truncated_CHANGELOG"
+    expected = File.read "./lib/new_commits_only.md"
+    expect(@creator.generate_log_for_new_commits(commits, changelog, "Java 0.99.0")).to eq expected
+  end
 
-  xit "creates formatted commits for the current release"
   xit "appends commits to an existing changelog"
   xit "checks that all the release commits are present"
   xit "thanks external contributors"
