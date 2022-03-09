522982b1855f87af06d550066ce3dc86e20b2680
Miranda Wilson <miranda@snowplowanalytics.com>
2022-03-09 22:25:20 +0000

    Add some methods

--- lib/changelog_creator.rb
+++ lib/changelog_creator.rb
@@ -4,9 +4,25 @@ puts "Hello, #{ARGV[0]}"
 
 class ChangelogCreator
 
+  COMMIT_PATTERN = /([\w\s\.,'"-:\`@]+)\((?:close|closes|fixes|fix) (\#\d+)\)/
+
   def read_commits(file_path)
-    file = File.read(file_path)
-    JSON.parse(file)
+    raise StandardError.new "Must be a JSON file" if file_path[-5..-1] != ".json"
+
+    JSON.parse(File.read(file_path))
+  end
+
+  def parse_message(message)
+    match = message.match(COMMIT_PATTERN)
+    match.nil? ? nil : "#{match[1]}(#{match[2]})"
+  end
+
+  def extract_messages(json)
+    p json.map { |commit| parse_message(commit["commit"]["message"]) }.compact
+  end
+
+  def make_header(version)
+    "#{version} (#{Date.today.strftime('%Y-%m-%d')})"
   end
 
 end
--- spec/changelog_creator_spec.rb
+++ spec/changelog_creator_spec.rb
@@ -1,22 +1,54 @@
+require "date"
 require "changelog_creator"
 
 describe ChangelogCreator do
 
+  before do
+    @creator = ChangelogCreator.new
+  end
+
   it "gets commits from a file" do
-    creator = ChangelogCreator.new
     filename = "./lib/example_commits.json"
-    results = creator.read_commits(filename)
+    results = @creator.read_commits(filename)
     expect(results[0]["commit"]["author"]["name"]).to eq "Miranda Wilson"
   end
 
-  xit "checks file is valid"
+  it "checks file extension is valid" do
+    filename = "./lib/changelog_creator.rb"
+    expect { @creator.read_commits(filename) }.to raise_error(StandardError)
+  end
 
-  xit "removes unwanted commits"
-  xit "strips extra parts of the commit message"
-  xit "gets the current date"
+  it "extracts the commit messages from saved JSON" do
+    filename = "./lib/example_commits.json"
+    json = @creator.read_commits(filename)
+    messages = @creator.extract_messages(json)
+    expect(messages.length).to eq 25
+    expect(messages[0]).to eq "Attribute community contributions in changelog (#289)"
+  end
+
+  it "parses commit messages" do
+    message = "Remove logging of user supplied values (close #286)\n\n* Remove this text"
+    expected = "Remove logging of user supplied values (#286)"
+    expect(@creator.parse_message(message)).to eq expected
+  end
+
+  it "returns nil if the commit message doesn't have an issue number" do
+    message = "Prepare for release"
+    expect(@creator.parse_message(message)).to be nil
+  end
+
+
+  it "makes a version header" do
+    version = "Java 0.12.0"
+    allow(Date).to receive(:today).and_return(Date.new(2022, 2, 1))
+    expect(@creator.make_header(version)).to eq "Java 0.12.0 (2022-02-01)"
+  end
+
+
   xit "creates formatted commits for the current release"
   xit "appends commits to an existing changelog"
   xit "checks that all the release commits are present"
   xit "thanks external contributors"
+  xit "can cope if there are brackets in the message"
 
 end
