ce02d47ca958587b12c3e114e20ee92cec9c0e5c
Miranda Wilson <miranda@snowplowanalytics.com>
2022-03-11 15:00:36 +0000

    Add rubocop

--- lib/changelog_creator.rb
+++ lib/changelog_creator.rb
@@ -1,30 +1,39 @@
 require "json"
+require "date"
 
 # puts "Hello, #{ARGV[0]}"
 
 class ChangelogCreator
-
-  COMMIT_PATTERN = /\A([\w\s\.,'"-:\`@]+)\((?:close|closes|fixes|fix) (\#\d+)\)$/
+  COMMIT_MESSAGE_PATTERN = /\A([\w\s.,'"-:`@]+)\((?:close|closes|fixes|fix) \#(\d+)\)$/
+  EMAIL_PATTERN = /\w+@(\w+.\w+)/
 
   def read_commits(file_path)
-    raise StandardError.new "Must be a JSON file" if file_path[-5..-1] != ".json"
+    raise StandardError, "Must be a JSON file" if file_path[-5..] != ".json"
 
     JSON.parse(File.read(file_path))
   end
 
   def read_changelog(file_path)
-    raise StandardError.new "Must be a changelog file" if file_path[-9..-1] != "CHANGELOG"
+    raise StandardError, "Must be a changelog file" if file_path[-9..] != "CHANGELOG"
 
     File.open(file_path).readlines.map(&:chomp)
   end
 
-  def parse_message(message)
-    match = message.match(COMMIT_PATTERN)
-    match.nil? ? nil : "#{match[1]}(#{match[2]})"
-  end
+  # def parse_commit_message(message)
+  #   match = message.match(COMMIT_MESSAGE_PATTERN)
+  #   match.nil? ? nil : "#{match[1]}(#{match[2]})"
+  # end
 
-  def extract_messages(json)
-    json.map { |commit| parse_message(commit["commit"]["message"]) }.compact
+  def process_single_commit(commit)
+    message_match = commit["commit"]["message"].match(COMMIT_MESSAGE_PATTERN)
+    return nil if message_match.nil?
+
+    p email_match = commit["commit"]["author"]["email"].match(EMAIL_PATTERN)
+
+    { message: message_match[1].strip,
+      issue: message_match[2],
+      author: commit["author"]["login"],
+      snowplower?: email_match.nil? ? false : true }
   end
 
   def make_header(version)
@@ -32,10 +41,18 @@ class ChangelogCreator
   end
 
   def generate_log_for_new_commits(commits_path, changelog_path, version)
-    commits = extract_messages(read_commits(commits_path))
+    commits = extract_commit_data(read_commits(commits_path))
     existing_changelog = read_changelog(changelog_path)
+
+    # Commits for the new version won't be in the changelog already
     new_commits = commits.take_while { |message| !existing_changelog.include? message }
-    "#{make_header(version)}\n-----------------------\n#{new_commits.join("\n")}\n"
+    text_block(new_commits, version)
   end
 
+  def text_block(array, version)
+    "#{make_header(version)}\n-----------------------\n#{array.join("\n")}\n"
+  end
 end
+
+# creator = ChangelogCreator.new
+# puts creator.generate_log_for_new_commits(ARGV[0], ARGV[1], "0.12.0")
--- spec/changelog_creator_spec.rb
+++ spec/changelog_creator_spec.rb
@@ -2,7 +2,6 @@ require "date"
 require "changelog_creator"
 
 describe ChangelogCreator do
-
   before do
     @creator = ChangelogCreator.new
     allow(Date).to receive(:today).and_return(Date.new(2022, 2, 1))
@@ -19,48 +18,63 @@ describe ChangelogCreator do
     expect { @creator.read_commits(filename) }.to raise_error(StandardError)
   end
 
-  it "extracts the commit messages from saved JSON" do
-    filename = "./lib/example_commits.json"
-    json = @creator.read_commits(filename)
-    messages = @creator.extract_messages(json)
-    expect(messages.length).to eq 25
-    expect(messages[0]).to eq "Attribute community contributions in changelog (#289)"
-    expect(messages[1]).to eq "Remove logging of user supplied values (#286)"
+  # it "parses commit messages" do
+  #   message = "Remove logging of user supplied values (close #286)\n\n* Remove this text"
+  #   expected = "Remove logging of user supplied values (#286)"
+  #   expect(@creator.parse_commit_message(message)).to eq expected
+  # end
+
+  it "parses commit JSON into a hash" do
+    filename = "./lib/single_commit_me.json"
+    parsed_json = @creator.read_commits(filename)
+
+    results = @creator.process_single_commit(parsed_json)
+    expect(results[:message]).to eq "Remove logging of user supplied values"
+    expect(results[:issue]).to eq "286"
+    expect(results[:author]).to eq "mscwilson"
+    expect(results[:snowplower?]).to eq true
   end
 
-  it "parses commit messages" do
-    message = "Remove logging of user supplied values (close #286)\n\n* Remove this text"
-    expected = "Remove logging of user supplied values (#286)"
-    expect(@creator.parse_message(message)).to eq expected
-  end
+  # it "extracts the commit data from saved JSON" do
+  #   filename = "./lib/example_commits.json"
+  #   json = @creator.read_commits(filename)
+  #   messages = @creator.extract_commit_data(json)
+  #   expect(messages.length).to eq 25
 
-  it "returns nil if the commit message doesn't have an issue number" do
-    message = "Prepare for release"
-    expect(@creator.parse_message(message)).to be nil
-  end
+  #   expect(messages[0][:message]).to eq "Attribute community contributions in changelog (#289)"
 
-  it "reads an existing changelog" do
-    filename = "./lib/example_CHANGELOG"
-    results = @creator.read_changelog(filename)
-    expect(results[0]).to eq "Java 0.11.0 (2021-12-14)"
-  end
+  #   expect(messages[1][:message]).to eq "Remove logging of user supplied values (#286)"
+  # end
 
-  # would be better to extract this from the tags
-  # use the GH API
-  it "makes a version header" do
-    version = "Java 0.12.0"
-    expect(@creator.make_header(version)).to eq "Java 0.12.0 (2022-02-01)"
-  end
+  # it "returns nil if the commit message doesn't have an issue number" do
+  #   message = "Prepare for release"
+  #   expect(@creator.parse_commit_message(message)).to be nil
+  # end
 
-  it "creates formatted commits for the current release" do
-    commits = "./lib/example_commits.json"
-    changelog = "./lib/truncated_CHANGELOG"
-    expected = File.read "./lib/new_commits_only.md"
-    expect(@creator.generate_log_for_new_commits(commits, changelog, "Java 0.99.0")).to eq expected
-  end
+  # it "reads an existing changelog" do
+  #   filename = "./lib/example_CHANGELOG"
+  #   results = @creator.read_changelog(filename)
+  #   expect(results[0]).to eq "Java 0.11.0 (2021-12-14)"
+  # end
 
-  xit "creates a new changelog file"
-  xit "thanks external contributors"
-  xit "gets the tags and commits using GH API"
+  # # would be better to extract this from the tags
+  # # use the GH API
+  # it "makes a version header" do
+  #   version = "Java 0.12.0"
+  #   expect(@creator.make_header(version)).to eq "Java 0.12.0 (2022-02-01)"
+  # end
 
+  # it "creates formatted commits for the current release" do
+  #   commits = "./lib/example_commits.json"
+  #   changelog = "./lib/truncated_CHANGELOG"
+  #   expected = File.read "./lib/new_commits_only.md"
+  #   expect(@creator.generate_log_for_new_commits(commits, changelog, "Java 0.99.0")).to eq expected
+  # end
+
+  # xit "creates a new changelog file"
+  # xit "produce something when there's no changelog already"
+  # xit "thanks external contributors"
+  # xit "gets the commits using GH API"
+  # xit "gets the version and date from tags using GH API"
+  # xit "works if more than like 30 commits to add (GH API doesn't automatically show all the commits)"
 end
