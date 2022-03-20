f60bef409bca7300d74bbc607d1ffb75dfb7cd1f
Miranda Wilson <miranda@snowplowanalytics.com>
2022-03-20 15:53:06 +0000

    Add a label for new branch

--- issue_labels/lib/issue_labels.rb
+++ issue_labels/lib/issue_labels.rb
@@ -7,12 +7,24 @@ client = Octokit::Client.new(access_token: ENV["ACCESS_TOKEN"])
 # puts user.fields
 # puts user.login
 
-# client.add_labels_to_an_issue("mscwilson/changelog-test", 11, ["I will be astonished if this works"])
+client.add_labels_to_an_issue("mscwilson/changelog-test", 11, ["I will be astonished if this works"])
 
 events = client.repository_events(ENV["GITHUB_REPOSITORY"])
 puts events.size
 
-recent_event = events[0]
-puts recent_event.fields
-puts recent_event.to_h
+recent_event = events[0].to_h
+
+BRANCH_NAME_PATTERN = /[0-9]+/
+
+if recent_event[:type] == "CreateEvent"
+  ref = recent_event[:payload][:ref]
+  type = recent_event[:payload][:ref_type]
+
+  if type == "branch"
+    issue_number = ref.match(BRANCH_NAME_PATTERN)[0]
+    client.add_labels_to_an_issue(ENV["GITHUB_REPOSITORY"], issue_number, ["type:in_progress"])
+  end
+
+end
+
 
