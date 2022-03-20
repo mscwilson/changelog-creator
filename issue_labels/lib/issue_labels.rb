888e902ba9f1c35ca9c37044d2d66e7757b21e60
Miranda Wilson <miranda@snowplowanalytics.com>
2022-03-20 15:14:21 +0000

    Investigate most recent event

--- issue_labels/lib/issue_labels.rb
+++ issue_labels/lib/issue_labels.rb
@@ -2,14 +2,13 @@ require "octokit"
 
 client = Octokit::Client.new(access_token: ENV["ACCESS_TOKEN"])
 
-user = client.user "mscwilson"
-puts user.name
-puts user.fields
-puts user.login
+# user = client.user "mscwilson"
+# puts user.name
+# puts user.fields
+# puts user.login
 
 # client.add_labels_to_an_issue("mscwilson/changelog-test", 11, ["I will be astonished if this works"])
 
-events = client.repository_events("mscwilson/changelog-test")
-puts events[0].payload
-
-puts ENV['GITHUB_REPOSITORY']
+events = client.repository_events(ENV['GITHUB_REPOSITORY'])
+puts events[0].fields
+puts events[0][:actor]
