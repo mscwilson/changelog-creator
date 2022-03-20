50cbe26f4a13f33d391ddd3642c557a2e5145083
Miranda Wilson <miranda@snowplowanalytics.com>
2022-03-20 11:48:14 +0000

    Add Octokit

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source "https://rubygems.org"
 
 ruby "~> 3.1"
 
+gem "octokit", "~> 4.0"
 gem "rspec"
 gem "rubocop"
--- issue_labels/lib/issue_labels.rb
+++ issue_labels/lib/issue_labels.rb
@@ -1,3 +1,14 @@
+require "octokit"
+
+client = Octokit::Client.new(access_token: ENV["ACCESS_TOKEN"])
+
+user = client.user "mscwilson"
+puts user.name
+puts user.fields
+puts user.login
+
+Octokit.add_labels_to_an_issue("mscwilson/changelog-test", 11, ["I will be astonished if this works"])
+
 puts "Hello from issue_labels"
 puts "Maybe this is? #{ENV['MY_ENV']}"
 puts "And this is? #{ENV['OTHER_ENV']}"
