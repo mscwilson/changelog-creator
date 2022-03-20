1a9e88d3251f950327981b291b6c153f02afce3e
Miranda Wilson <miranda@snowplowanalytics.com>
2022-03-20 14:54:17 +0000

    Print ENV

--- issue_labels/lib/issue_labels.rb
+++ issue_labels/lib/issue_labels.rb
@@ -7,8 +7,9 @@ puts user.name
 puts user.fields
 puts user.login
 
-client.add_labels_to_an_issue("mscwilson/changelog-test", 11, ["I will be astonished if this works"])
+# client.add_labels_to_an_issue("mscwilson/changelog-test", 11, ["I will be astonished if this works"])
 
-puts "Hello from issue_labels"
-puts "Maybe this is? #{ENV['MY_ENV']}"
-puts "And this is? #{ENV['OTHER_ENV']}"
+events = client.repository_events("mscwilson/changelog-test")
+puts events[0]
+
+puts ENV['GITHUB_EVENT_NAME']
