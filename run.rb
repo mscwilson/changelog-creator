1ed888685625612d80f6aaa7ea1b5268ecf96570
Miranda Wilson <miranda@snowplowanalytics.com>
2022-03-30 10:19:27 +0100

    Add tests (close #25)
    * Make a new CHANGELOG if there isn't one already
    
    * Use different commit message if a new LOG is being created
    
    * Get 60 commits instead of 30

--- run.rb
+++ run.rb
@@ -9,7 +9,7 @@ def run
 
   client = Octokit::Client.new(access_token: "secret")
 
-  connection = GithubApiConnection.new(client: client, repo_name: "mscwilson/try-out-actions-here")
+  connection = GithubApiConnection.new(client:, repo_name: "mscwilson/try-out-actions-here")
   creator = ChangelogCreator.new
 
   events = connection.repo_events
@@ -18,17 +18,33 @@ def run
     return
   end
 
+  commit_new_changelog(connection, creator)
+end
+
+def commit_new_changelog(connection, creator)
   branches = connection.pr_branches(events[0])
   commits = connection.commits_from_branch(branch_name: branches[:head_ref])
-  existing_changelog = connection.get_file(path: LOG_PATH)
-  new_log_section = creator.simple_changelog_block(branch_name: branches[:head_ref], commits: commits)
+
+  changelog_exists = true
+  begin
+    existing_changelog = connection.get_file(path: LOG_PATH)
+  rescue Octokit::NotFound
+    puts "No existing CHANGELOG found."
+    existing_changelog = { sha: nil, contents: "" }
+    changelog_exists = false
+  end
+
+  new_log_section = creator.simple_changelog_block(branch_name: branches[:head_ref], commits:)
   updated_log = "#{new_log_section}\n#{existing_changelog[:contents]}"
 
-  connection.update_file(commit_message: "Update CHANGELOG",
+  commit_message = changelog_exists ? "Update CHANGELOG" : "Create CHANGELOG"
+  connection.update_file(commit_message:,
                          file_contents: updated_log,
                          file_path: LOG_PATH,
                          sha: existing_changelog[:sha],
                          branch: branches[:head_ref])
+
+  puts changelog_exists ? "CHANGELOG updated." : "CHANGELOG created."
 end
 
 run
