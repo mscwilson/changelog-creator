4c7714b385726093b2ab186f202afef16a1a3d12
Miranda Wilson <miranda@snowplowanalytics.com>
2022-03-31 14:17:55 +0100

    Comment out almost everything in run script

--- run.rb
+++ run.rb
@@ -6,58 +6,62 @@ require "./lib/changelog_creator"
 
 LOG_PATH = "./CHANGELOG"
 
-def run
-  puts "Starting Changelog Creator."
+puts "Hello from run.rb"
 
-  creator = ChangelogCreator.new
+# def run
+#   puts "Starting Changelog Creator."
 
-  events = creator.octokit.repo_events
-  unless creator.octokit.pr_opened_to_main?(events)
-    puts "No action taken."
-    return
-  end
+#   creator = ChangelogCreator.new
 
-  branches = creator.octokit.pr_branches(events[0])
-  commits = creator.octokit.commits_from_branch(branch_name: branches[:head_ref])
-  commit_data = creator.extract_relevant_commit_data(commits)
+#   events = creator.octokit.repo_events
+#   unless creator.octokit.pr_opened_to_main?(events)
+#     puts "No action taken."
+#     return
+#   end
 
-  pr_number = events[0]["payload"]["number"]
+#   branches = creator.octokit.pr_branches(events[0])
+#   commits = creator.octokit.commits_from_branch(branch_name: branches[:head_ref])
+#   commit_data = creator.extract_relevant_commit_data(commits)
 
-  commit_changelog_file(creator, branches[:head_ref], commit_data)
+#   pr_number = events[0]["payload"]["number"]
 
-  formatted_log = creator.fancy_changelog(commit_data:)
-  creator.octokit.comment_on_pr_or_issue(number: pr_number, text: formatted_log)
-  puts "Formatted changelog added as comment to PR ##{pr_number}"
+#   commit_changelog_file(creator, branches[:head_ref], commit_data)
 
-  puts "Action completed."
-end
+#   formatted_log = creator.fancy_changelog(commit_data:)
+#   creator.octokit.comment_on_pr_or_issue(number: pr_number, text: formatted_log)
+#   puts "Formatted changelog added as comment to PR ##{pr_number}"
 
-def commit_changelog_file(creator, branch_name, commits)
-  puts "Getting CHANGELOG file."
-  changelog_exists = true
-  begin
-    existing_changelog = creator.octokit.get_file(path: LOG_PATH)
-  rescue Octokit::NotFound
-    puts "No existing CHANGELOG found."
-    existing_changelog = { sha: nil, contents: "" }
-    changelog_exists = false
-  end
+#   puts "Action completed."
+# end
 
-  new_log_section = creator.simple_changelog_block(branch_name:, commit_data: commits)
-  updated_log = "#{new_log_section}\n#{existing_changelog[:contents]}"
+# def commit_changelog_file(creator, branch_name, commits)
+#   puts "Getting CHANGELOG file."
+#   changelog_exists = true
+#   begin
+#     existing_changelog = creator.octokit.get_file(path: LOG_PATH)
+#   rescue Octokit::NotFound
+#     puts "No existing CHANGELOG found."
+#     existing_changelog = { sha: nil, contents: "" }
+#     changelog_exists = false
+#   end
 
-  if new_log_section.empty?
-    puts "No version number found. No CHANGELOG file created."
-  else
-    commit_message = changelog_exists ? "Update CHANGELOG" : "Create CHANGELOG"
-    creator.octokit.update_file(commit_message:,
-                                file_contents: updated_log,
-                                file_path: LOG_PATH,
-                                sha: existing_changelog[:sha],
-                                branch: branch_name)
+#   new_log_section = creator.simple_changelog_block(branch_name:, commit_data: commits)
+#   updated_log = "#{new_log_section}\n#{existing_changelog[:contents]}"
 
-    puts changelog_exists ? "CHANGELOG updated." : "CHANGELOG created."
-  end
-end
+#   if new_log_section.empty?
+#     puts "No version number found. No CHANGELOG file created."
+#   else
+#     commit_message = changelog_exists ? "Update CHANGELOG" : "Create CHANGELOG"
+#     creator.octokit.update_file(commit_message:,
+#                                 file_contents: updated_log,
+#                                 file_path: LOG_PATH,
+#                                 sha: existing_changelog[:sha],
+#                                 branch: branch_name)
 
-run
+#     puts changelog_exists ? "CHANGELOG updated." : "CHANGELOG created."
+#   end
+# end
+
+# run
+
+puts "hello"
