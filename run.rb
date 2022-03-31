624299cbb63940506f60b26f3e01c64a8de3bb0f
Miranda Wilson <miranda@snowplowanalytics.com>
2022-03-31 14:23:58 +0100

    Restore actual code

--- run.rb
+++ run.rb
@@ -6,62 +6,58 @@ require "./lib/changelog_creator"
 
 LOG_PATH = "./CHANGELOG"
 
-puts "Hello from run.rb"
+def run
+  puts "Starting Changelog Creator."
 
-# def run
-#   puts "Starting Changelog Creator."
+  creator = ChangelogCreator.new
 
-#   creator = ChangelogCreator.new
+  events = creator.octokit.repo_events
+  unless creator.octokit.pr_opened_to_main?(events)
+    puts "No action taken."
+    return
+  end
 
-#   events = creator.octokit.repo_events
-#   unless creator.octokit.pr_opened_to_main?(events)
-#     puts "No action taken."
-#     return
-#   end
+  branches = creator.octokit.pr_branches(events[0])
+  commits = creator.octokit.commits_from_branch(branch_name: branches[:head_ref])
+  commit_data = creator.extract_relevant_commit_data(commits)
 
-#   branches = creator.octokit.pr_branches(events[0])
-#   commits = creator.octokit.commits_from_branch(branch_name: branches[:head_ref])
-#   commit_data = creator.extract_relevant_commit_data(commits)
+  pr_number = events[0]["payload"]["number"]
 
-#   pr_number = events[0]["payload"]["number"]
+  commit_changelog_file(creator, branches[:head_ref], commit_data)
 
-#   commit_changelog_file(creator, branches[:head_ref], commit_data)
+  formatted_log = creator.fancy_changelog(commit_data:)
+  creator.octokit.comment_on_pr_or_issue(number: pr_number, text: formatted_log)
+  puts "Formatted changelog added as comment to PR ##{pr_number}"
 
-#   formatted_log = creator.fancy_changelog(commit_data:)
-#   creator.octokit.comment_on_pr_or_issue(number: pr_number, text: formatted_log)
-#   puts "Formatted changelog added as comment to PR ##{pr_number}"
+  puts "Action completed."
+end
 
-#   puts "Action completed."
-# end
+def commit_changelog_file(creator, branch_name, commits)
+  puts "Getting CHANGELOG file."
+  changelog_exists = true
+  begin
+    existing_changelog = creator.octokit.get_file(path: LOG_PATH)
+  rescue Octokit::NotFound
+    puts "No existing CHANGELOG found."
+    existing_changelog = { sha: nil, contents: "" }
+    changelog_exists = false
+  end
 
-# def commit_changelog_file(creator, branch_name, commits)
-#   puts "Getting CHANGELOG file."
-#   changelog_exists = true
-#   begin
-#     existing_changelog = creator.octokit.get_file(path: LOG_PATH)
-#   rescue Octokit::NotFound
-#     puts "No existing CHANGELOG found."
-#     existing_changelog = { sha: nil, contents: "" }
-#     changelog_exists = false
-#   end
+  new_log_section = creator.simple_changelog_block(branch_name:, commit_data: commits)
+  updated_log = "#{new_log_section}\n#{existing_changelog[:contents]}"
 
-#   new_log_section = creator.simple_changelog_block(branch_name:, commit_data: commits)
-#   updated_log = "#{new_log_section}\n#{existing_changelog[:contents]}"
+  if new_log_section.empty?
+    puts "No version number found. No CHANGELOG file created."
+  else
+    commit_message = changelog_exists ? "Update CHANGELOG" : "Create CHANGELOG"
+    creator.octokit.update_file(commit_message:,
+                                file_contents: updated_log,
+                                file_path: LOG_PATH,
+                                sha: existing_changelog[:sha],
+                                branch: branch_name)
 
-#   if new_log_section.empty?
-#     puts "No version number found. No CHANGELOG file created."
-#   else
-#     commit_message = changelog_exists ? "Update CHANGELOG" : "Create CHANGELOG"
-#     creator.octokit.update_file(commit_message:,
-#                                 file_contents: updated_log,
-#                                 file_path: LOG_PATH,
-#                                 sha: existing_changelog[:sha],
-#                                 branch: branch_name)
+    puts changelog_exists ? "CHANGELOG updated." : "CHANGELOG created."
+  end
+end
 
-#     puts changelog_exists ? "CHANGELOG updated." : "CHANGELOG created."
-#   end
-# end
-
-# run
-
-puts "hello"
+run
