a924ef4f9a52ec02ada6e2c2c26035e597db11fd
Miranda Wilson <miranda@snowplowanalytics.com>
2022-04-01 20:25:10 +0100

    Add fancylog output to action

--- run.rb
+++ run.rb
@@ -1,8 +1,9 @@
-# require "dotenv/load"
+require "dotenv/load"
 require "octokit"
 
 require "./lib/github_api_connection"
 require "./lib/changelog_creator"
+require "./lib/manager"
 
 LOG_PATH = "./CHANGELOG"
 
@@ -10,26 +11,26 @@ def run
   puts "Starting Changelog Creator."
 
   creator = ChangelogCreator.new
+  manager = Manager.new
 
   events = creator.octokit.repo_events
-  unless creator.octokit.pr_opened_to_main?(events)
+  unless manager.pr_branches_release_and_main?
     puts "No action taken."
     return
   end
 
-  branches = creator.octokit.pr_branches(events[0])
-  commits = creator.octokit.commits_from_branch(branch_name: branches[:head_ref])
+  commits = creator.octokit.commits_from_branch(branch_name: ENV["GITHUB_HEAD_REF"])
   commit_data = creator.extract_relevant_commit_data(commits)
 
   pr_number = events[0]["payload"]["number"]
 
   # commit_changelog_file(creator, branches[:head_ref], commit_data)
 
-  formatted_log = creator.fancy_changelog(commit_data:)
-  creator.octokit.comment_on_pr_or_issue(number: pr_number, text: formatted_log)
-  puts "Formatted changelog added as comment to PR ##{pr_number}"
+  # formatted_log = creator.fancy_changelog(commit_data:)
+  # creator.octokit.comment_on_pr_or_issue(number: pr_number, text: formatted_log)
+  # puts "Formatted changelog added as comment to PR ##{pr_number}"
 
-  puts "Action completed."
+  # puts "Action completed."
 end
 
 def commit_changelog_file(creator, branch_name, commits)
