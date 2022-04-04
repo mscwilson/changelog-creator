e8023b5ddecc242f9fe5678aecb94ed3f3796bdc
Miranda Wilson <miranda@snowplowanalytics.com>
2022-04-04 17:58:20 +0100

    Restore commented out bit

--- run.rb
+++ run.rb
@@ -13,19 +13,19 @@ def run
   creator = ChangelogCreator.new
   manager = Manager.new
 
-  # pr_action = manager.pr_branches_release_and_main?
+  pr_action = manager.pr_branches_release_and_main?
 
-  # # Commit a new CHANGELOG file into the release branch
-  # if pr_action
-  #   commits = creator.octokit.commits_from_branch(branch_name: ENV["GITHUB_HEAD_REF"])
-  #   commit_data = creator.relevant_commit_data(commits)
-  #   commit_changelog_file(creator, ENV["GITHUB_HEAD_REF"], commit_data)
-  #   puts "Action completed."
-  #   puts
-  #   puts Base64.strict_encode64("No release notes needed!")
-  #   nil
+  # Commit a new CHANGELOG file into the release branch
+  if pr_action
+    commits = creator.octokit.commits_from_branch(branch_name: ENV["GITHUB_HEAD_REF"])
+    commit_data = creator.relevant_commit_data(commits)
+    commit_changelog_file(creator, ENV["GITHUB_HEAD_REF"], commit_data)
+    puts "Action completed."
+    puts
+    puts Base64.strict_encode64("No release notes needed!")
+    nil
 
-  # else
+  else
     # Output release notes to use as part of a GH deploy workflow
     # Working on the assumption that the release PR was the most recently made (highest number)
     # Not necessarily true
@@ -44,7 +44,7 @@ def run
     puts "Action completed."
     puts
     puts Base64.strict_encode64(release_notes)
-  # end
+  end
 end
 
 def commit_changelog_file(creator, branch_name, commits)
